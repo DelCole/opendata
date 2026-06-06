51f62e94a6129a1d56fcca1c1c764341905c0370
jhordycg <jhordycg@pm.me>
2026-06-06 06:39:08 -0500

    feat: update dependencies and enhance server setup with Hono framework

--- main.ts
+++ main.ts
@@ -1,5 +1,30 @@
-import { createApp } from "@jhordycg/json-server";
+import { Hono } from "@hono/hono";
+import { cache } from "@hono/hono/cache";
+import { showRoutes } from "@hono/hono/dev";
+import { etag, RETAINED_304_HEADERS } from "@hono/hono/etag";
+import { jsonServerFactory } from "@jhordycg/json-server";
 
-const app = await createApp("db.json");
+const factory = await jsonServerFactory("db.json");
+
+const app = new Hono()
+  .use(etag({ retainedHeaders: RETAINED_304_HEADERS }))
+  .use(
+    cache({
+      wait: true,
+      cacheName: "opendata-api",
+      cacheControl: "public, max-age=3153600, stale-while-revalidate=86400, immutable",
+    }),
+  )
+  .use((_, next) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(async () => {
+        await next();
+        resolve();
+      }, 750);
+    });
+  })
+  .route("/", factory.createApp());
+
+showRoutes(app);
 
 export default { fetch: app.fetch };
