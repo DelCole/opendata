4200e87f728b33325b7b66af17392ff5e87db850
jhordycg <jhordycg@pm.me>
2026-06-06 20:58:40 -0500

    feat: and cors configuration

--- main.ts
+++ main.ts
@@ -1,18 +1,25 @@
 import { Hono } from "@hono/hono";
 import { cache } from "@hono/hono/cache";
+import { cors } from "@hono/hono/cors";
 import { showRoutes } from "@hono/hono/dev";
 import { etag, RETAINED_304_HEADERS } from "@hono/hono/etag";
 import { jsonServerFactory } from "@jhordycg/json-server";
 
 const factory = await jsonServerFactory("db.json");
+const CACHE_CONTROL =
+  "public,max-age=3153600,stale-while-revalidate=86400,immutable";
+const ALLOWED_ORIGINS = Deno.env.get("APP_ALLOWED_ORIGIN")?.split(";") ?? [];
+
+console.log(ALLOWED_ORIGINS);
 
 const app = new Hono()
+  .use(cors({ origin: ALLOWED_ORIGINS, maxAge: 3, allowMethods: ["GET"] }))
   .use(etag({ retainedHeaders: RETAINED_304_HEADERS }))
   .use(
     cache({
       wait: true,
       cacheName: "opendata-api",
-      cacheControl: "public, max-age=3153600, stale-while-revalidate=86400, immutable",
+      cacheControl: CACHE_CONTROL,
     }),
   )
   .use((_, next) => {
